905fad504695cdc6b275de9420ed06e2543570a8
HorsemanDj <djgoe1990@gmail.com>
2023-06-21 02:25:44 +0200

    Update gettoni-plugin.php

--- gettoni-plugin.php
+++ gettoni-plugin.php
@@ -1,5 +1,3 @@
-/
-
 <?php
 /*
 Plugin Name: Gettoni Plugin
@@ -9,25 +7,17 @@ Version: 1
 Author: Riccardi Gaetano
 Author URI: https://www.riccardigaetano.it
 
-Licenza:
-
-Copyright (c) 2023, Riccardi Gaetano
-
-Tutti i diritti sono riservati.
-
-Il Gettoni Plugin è un software proprietario.
-
-È severamente proibito il riutilizzo, la copia, la modifica o la distribuzione di questo software senza il permesso scritto e esplicito dell'autore.
-
-Il software è fornito "così com'è", senza alcuna garanzia, esplicita o implicita, inclusa, ma non limitata a, garanzie di commerciabilità, idoneità per uno scopo specifico e non violazione.
-
-In nessun caso l'autore o i titolari del copyright saranno responsabili per qualsiasi reclamo, danni o altre responsabilità, sia in un'azione di contratto, responsabilità civile o altro, derivante da, fuori o in connessione con il software o l'uso o altre operazioni nel software.
-
-Per ottenere il permesso di utilizzare il software oltre i limiti di questa licenza, si prega di contattare Riccardi Gaetano a info@riccardigaetano.it.
-
+# License
+Copyright (c) 2023, Riccardi Gaetano.
+All rights reserved.
+The Gettoni Plugin is proprietary software.
+Any reuse, copying, modification, or distribution of this software without the explicit written permission of the author is strictly prohibited.
+The software is provided "as is", without any warranty, express or implied, including but not limited to warranties of merchantability, fitness for a particular purpose, and non-infringement.
+In no event shall the author or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
+To obtain permission to use the software beyond the limits of this license, please contact Riccardi Gaetano at info@riccardigaetano.it.
 */
 
-/// Azione all'attivazione del plugin
+// Azione all'attivazione del plugin
 register_activation_hook( __FILE__, 'gettoni_plugin_activate' );
 function gettoni_plugin_activate() {
     // Creazione della voce "gettone" per tutti gli utenti esistenti
